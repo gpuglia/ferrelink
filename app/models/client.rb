3a942f9bc1c49875dea20a2ac17da8d3ce226b49
Gustavo Puglia <gpugliac@gmail.com>
2015-01-31 20:17:11 +0000

    Remove Client model

--- app/models/client.rb
+++
@@ -1,8 +0,0 @@
-class Client < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  validates :name, :rif, :address, presence: true
-end
