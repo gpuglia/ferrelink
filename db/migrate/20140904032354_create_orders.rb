99568ddaf5da2515c0e774922038ccd453d42377
Gustavo Puglia <gpugliac@gmail.com>
2015-01-31 20:53:09 +0000

    Add new fields to orders

--- db/migrate/20140904032354_create_orders.rb
+++ db/migrate/20140904032354_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-      t.integer :salesman_id
+      t.integer :salesman_id, :code, :bundle, :minimum_sale
+      t.string :code, :description, :unit
 
       t.timestamps
     end
