7c79defb5b46c80b6abf739c6c84f9ed6c724f63
Gustavo Puglia <gpugliac@gmail.com>
2015-05-09 01:41:06 +0100

    spec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'shoulda/matchers'
 require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
@@ -14,6 +13,14 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.mock_with :rspec
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.infer_spec_type_from_file_location!
+
   config.before(:suite)   do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.clean
