4590bc4bcb246b59c827e4e481776e5999d67025
Gustavo Puglia <gpugliac@gmail.com>
2015-05-10 01:00:35 +0100

    Add associates to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ActiveRecord::Base
   belongs_to :salesman
   has_many :items, dependent: :destroy
+  has_many :products, through: :items
 
-  accepts_nested_attributes_for :items
+  accepts_nested_attributes_for :items, reject_if: -> (attr) { attrs['quantity'].blank? }
 end
